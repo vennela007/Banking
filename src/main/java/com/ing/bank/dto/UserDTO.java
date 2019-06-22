74d50be2e16e488c367a20e19304fe883a8db5d5
himajareddy506 <himajareddy.cse@gmail.com>
2019-06-22 16:01:08 +0530

    list of approvals shashank

---
+++ src/main/java/com/ing/bank/dto/UserDTO.java
@@ -0,0 +1,35 @@
+package com.ing.bank.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class UserDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Long userId;
+	private String userName;
+	private String accountType;
+	private Long mobileNumber;
+	private String email;
+	private String address;
+	private String nominee;
+	private String nomineeRelation;
+	private int age;
+	private LocalDate creationDate;
+	private Long aadharNumber;
+	private String loginName;
+	private String password;
+	private String role;
+	private String status;
+
+}
