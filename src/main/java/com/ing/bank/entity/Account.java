384f537288f52e4f7acb4ed94513925534cdd18d
bindubogarapu <bogarapu.bindu@gmail.com>
2019-06-22 00:20:11 -0700

    added account

---
+++ src/main/java/com/ing/bank/entity/Account.java
@@ -0,0 +1,41 @@
+package com.ing.bank.entity;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Account implements Serializable{/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long accountId;
+	private String accountNumber;
+	private String accountType;
+	private LocalDate creationDate;
+	private double balance;
+	private String status;
+	@OneToOne
+	@JoinColumn(name="customerId")
+	private Customer customer;
+
+}
+
