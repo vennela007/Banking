530121f2618c9eb4999f4756b9ff14b272b382d3
prabhu alam <rudraksh.prabhu@gmail.com>
2019-06-23 13:18:01 +0530

    Entity and Dto updated

---
+++ src/main/java/com/ing/bank/dto/TransactionDTO.java
@@ -0,0 +1,15 @@
+package com.ing.bank.dto;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TransactionDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+}
--- src/main/java/com/ing/bank/entity/Transaction.java
+++ src/main/java/com/ing/bank/entity/Transaction.java
@@ -2,7 +2,6 @@ package com.ing.bank.entity;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,11 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,23 +23,22 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 
-public class Account implements Serializable {
+public class Transaction implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long accountId;
-	private String accountNumber;
-	private String accountType;
-	private LocalDate creationDate;
-	private double balance;
+	private Long transactionId;
+	private Long fromAccount;
+	private Long toAccount;
+	private LocalDate transactionDate;
+	private Double transactionAmount;
+	private String remarks;
+	private String transactionType;
 	@JsonBackReference
 	@ManyToOne
-	@JoinColumn(name = "userId")
-	private User user;
-	
-	@JsonManagedReference
-	@OneToMany(mappedBy = "account")
-	private List<Transaction> transactions;
+	@JoinColumn(name = "accountId")
+	Account account;
 
 }
